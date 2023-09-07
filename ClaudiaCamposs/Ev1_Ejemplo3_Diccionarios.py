cc96d3223dd00043a64f5ab5fdbdd86aba544f7c
ClaudiaCamposs <142376014+ClaudiaCamposs@users.noreply.github.com>
2023-09-06 23:34:01 -0600

    Add files via upload

---
+++ ClaudiaCamposs/Ev1_Ejemplo3_Diccionarios.py
@@ -0,0 +1,15 @@
+# Ejemplo 3: Diccionarios
+
+libros={
+    10:"El diario de Ana Frank",
+    20:"The Maze Runner",
+    30:"Las ventajas de ser invisible",
+    40:"Harry Potter",
+    50:"Bajo la misma estrella",
+}
+
+print(len(libros))
+print(libros.keys())
+print(libros.values())
+
+print(type(libros))
