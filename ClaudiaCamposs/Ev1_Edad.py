3bd81f05ab9f2ac6bba22a38a5ba38c2b7c7ad22
ClaudiaCamposs <142376014+ClaudiaCamposs@users.noreply.github.com>
2023-09-06 22:52:28 -0600

    Delete ClaudiaCamposs/Ev1_Edad.py

--- ClaudiaCamposs/Ev1_Edad.py
+++
@@ -1,9 +0,0 @@
-# Soloicitar la edad y definir si es mayor o menor de edad
-
-edad=int(input("Ingrese su edad: "))
-if edad<0:
-    print("Dato incorrecto. Ingrese una edad vailida.")
-elif edad>=18:
-    print("Es mayor de edad.")
-else:
-    print('Es menor de edad.')
